7daaf8ad67c38f7bb516760fd0727757e741c88a
Andy Tran <hello@andytran.me>
2019-06-17 02:22:02 -0700

    Added in build script.

---
+++ src/components/index.js
@@ -0,0 +1,55 @@
+export { Badge } from './Badge';
+
+export { Bold } from './Bold';
+
+export { Breadcrumb } from './Badge';
+
+export { Button } from './Badge';
+
+export { ButtonGroup } from './Badge';
+
+export { Card } from './Badge';
+
+export { FeaturedPost } from './Badge';
+
+export { Field } from './Badge';
+
+export { Footer } from './Badge';
+
+export { Form } from './Badge';
+
+export { GlobalStyles } from './Badge';
+
+export { Header } from './Badge';
+
+export { Heading } from './Badge';
+
+export { Icon } from './Badge';
+
+export { Image } from './Badge';
+
+export { Input } from './Badge';
+
+export { Layout } from './Badge';
+
+export { Link } from './Badge';
+
+export { List } from './Badge';
+
+export { Logo } from './Badge';
+
+export { Menu } from './Badge';
+
+export { Navigation } from './Badge';
+
+export { NewsletterWidget } from './Badge';
+
+export { PageHeader } from './Badge';
+
+export { Paragraph } from './Badge';
+
+export { Post } from './Badge';
+
+export { Tabs } from './Badge';
+
+export { Widget } from './Badge';
