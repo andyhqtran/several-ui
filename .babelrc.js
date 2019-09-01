9b6076bdb56a6172c4f110d579c44735a19d0cc3
Andy Tran <andy@mixmax.com>
2019-09-01 00:41:19 -0700

    Updated babelrc.

--- .babelrc.js
+++ .babelrc.js
@@ -1,15 +1,16 @@
 module.exports = {
   "presets": [
-    [
-      '@babel/preset-env',
-      {
-        "loose": true,
-        "modules": process.env.BABEL_ENV === 'cjs' ? 'commonjs' : false,
-      },
-    ],
+    '@babel/preset-env',
     '@babel/preset-react',
   ],
   'plugins': [
     '@babel/plugin-proposal-export-default-from',
+    [
+      'babel-plugin-styled-components',
+      {
+        'displayName': true,
+        'ssr': true,
+      },
+    ],
   ],
 };
