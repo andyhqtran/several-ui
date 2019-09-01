221351af2aaa8aa6351ad9050fc743c725c3a7f2
Andy Tran <andy@mixmax.com>
2019-09-01 00:41:11 -0700

    Removed linebreak in between each export.

--- src/components/index.js
+++ src/components/index.js
@@ -1,63 +1,32 @@
 export Avatar from './Avatar';
-
 export Badge from './Badge';
-
 export Bold from './Bold';
-
 export Breadcrumb from './Breadcrumb';
-
 export Button from './Button';
-
 export ButtonGroup from './ButtonGroup';
-
 export Card from './Card';
-
 export ContentLayout from './ContentLayout';
-
 export FeaturedPost from './FeaturedPost';
-
 export Field from './Field';
-
 export Footer from './Footer';
-
 export Form from './Form';
-
 export GlobalStyles from './GlobalStyles';
-
 export Grid from './Grid';
-
 export Header from './Header';
-
 export Heading from './Heading';
-
 export Hero from './Hero';
-
 export Icon from './Icon';
-
 export Image from './Image';
-
 export Input from './Input';
-
 export Link from './Link';
-
 export List from './List';
-
 export Logo from './Logo';
-
 export Menu from './Menu';
-
 export Navigation from './Navigation';
-
 export NewsletterWidget from './NewsletterWidget';
-
 export PageHeader from './PageHeader';
-
 export Paragraph from './Paragraph';
-
 export Post from './Post';
-
 export Tabs from './Tabs';
-
 export User from './User';
-
 export Widget from './Widget';
